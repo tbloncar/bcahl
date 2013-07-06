afbd277529b26488dc4f2d361687604c63de2a35
Travis Loncar <loncar.travis@gmail.com>
2013-07-06 11:27:15 -0400

    Set up environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Bcahl::Application.config.secret_key_base = '6d95e45bb8838454e84d56658cce2640ab00cd19537ed60358075196434d4244f50654e7f0e9b844a80fd53bc52efd2a31fa39a46537e4dd649e81ed2cc449f3'
+Bcahl::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
