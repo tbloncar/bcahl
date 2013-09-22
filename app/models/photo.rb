0347d6c731a49eb2b7b6d4961822239f199aae39
Travis Loncar <loncar.travis@gmail.com>
2013-09-22 03:16:30 -0400

    Add uniqueness validation to Photo.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,7 @@ class Photo < ActiveRecord::Base
 	
 	has_attached_file :photo, :styles => { :large => "400x400>", :medium => "300x300>", :thumb => "100x100>" }
 
+	validates :photo, uniqueness: true
+
 	scope :latest, -> { limit(5) }
 end
